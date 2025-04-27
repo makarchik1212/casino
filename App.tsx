82b39e850dba9f6d82d8a8309c215122ce9e7bfa
makarchik1212 <makarchikloh@gmail.com>
2025-04-27 17:33:20 +0300

    Create App.tsx

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div style={{ padding: 20, fontFamily: "sans-serif" }}>
+      <h1>Welcome to Casino1</h1>
+      <p>Мини-приложение: с UI и простым API на Express.</p>
+    </div>
+  );
+}
